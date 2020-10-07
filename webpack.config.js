36cd770ab52c963af4a5fbfc5a5b2fbbd85b6ddd
yuchuan <yuchuan@goodsogood.com>
2020-10-07 16:13:40 +0800

    feat: 完善配置

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,14 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: "development",
+  devtool: 'source-map',
   entry: ["./src/index.js"],
   output: {
     path: join(__dirname, "dist"),
     filename: "bundle.js"
+  },
+  resolve: {
+    alias: { 'react-dom': '@hot-loader/react-dom' }
   },
   module: {
     rules: [
@@ -15,6 +19,44 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: ['babel-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "css-loader",
+        ]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            outputPath: 'images/',
+          }
+        }
+      },
+      {
+        test: /\.(eot|woff2?|ttf|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: '[name]-[hash:5].min.[ext]',
+              limit: 5000,
+              publicPath: 'fonts/',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
       }
     ]
   },
@@ -31,6 +73,5 @@ module.exports = {
     contentBase: join(__dirname, "./dist"),
     host: "0.0.0.0",
     port: 3000,
-    historyApiFallback: true,
   }
 }
